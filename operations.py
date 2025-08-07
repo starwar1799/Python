5352c1fb40b1a4795566f4f4f6d9ef0155cfc905
starwar1799 <starwar1799@gmail.com>
2025-08-07 11:29:25 -0500

    Averages

---
+++ operations.py
@@ -0,0 +1,11 @@
+#Doing operations
+tree1=5
+tree2=4
+tree3=3
+tree4=2
+tree5=1
+addition=tree1+tree2+tree3+tree4+tree5
+average=addition/5
+
+print("The sum of all 5 trees is...", addition)
+print("The average of all 5 trees is...", average)
