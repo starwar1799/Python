683008f7a15f31a9527e8219c18261f608e07850
starwar1799 <starwar1799@gmail.com>
2025-08-08 12:22:21 -0500

    Temperature

---
+++ Homework.py
@@ -0,0 +1,14 @@
+#Taking total amount as user input
+Input = int(input("Enter number."))
+Divisible= Input/Input*Input
+SquareRoot=Input*Divisible
+
+print("Answer is...",SquareRoot)
+
+A= 50
+Temperature = input("enter temperature.")
+
+if (Temperature>A):
+    print("Dress warm, folks!")
+else:
+     print("It'll be a c-c-cold day.")
