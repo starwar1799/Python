a8d71a074b4762c158049b8ceead71951bc76542
starwar1799 <starwar1799@gmail.com>
2025-08-08 11:58:41 -0500

    Calulate Profit

--- ConditionalStatements.py
+++ ConditionalStatements.py
@@ -5,3 +5,19 @@ if (A>B):
     print(A,"is greater than", B)
 else:
      print(A,"is less than", B)
+
+
+#CalculatingProfit
+costprice =int(input("enter the cp: "))
+
+sellingprice =int(input("enter the sp: "))
+
+if(sellingprice>costprice):
+
+   print("profit")
+
+   pt=sellingprice-costprice
+   print(pt)
+
+else :
+   print("HAHHAHAHAHHAHAHAH LMAO NO PROFIT")
