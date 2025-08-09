59967d26141d16b4f7fad4470b274b2474d241b9
starwar1799 <starwar1799@gmail.com>
2025-08-09 12:00:38 -0500

    Divisible Numbers

--- Operatoprecedence.py
+++ Operatoprecedence.py
@@ -6,3 +6,13 @@ if Name== "Srikara" or Name== "Sreshta":
     print("Hello")
 else:
     print("GETTT OUTTTTT")
+
+
+#Divisible Numbers
+Numerator= int(input("Enter numerator: "))
+Denominator= int(input("Enter denominator: "))
+
+if Numerator%Denominator==0:
+    print("Numerator is divisble by denominator")
+else:
+    print("Numerator is NOT divisible by denominator")
